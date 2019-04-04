7553cdcb947ff2b6130cb9ea5f8fec4b841385f0
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-04 19:47:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(fizz_3)
+  if fizz_3 % 3 == 0 && fizz_3 % 5 ==0
+    "FizzBuzz"
+  elsif fizz_3 % 3 == 0 
+    "Fizz"
+  elsif fizz_3 % 5 ==0 
+    "Buzz"
+  end
+end 
+ 
+   
